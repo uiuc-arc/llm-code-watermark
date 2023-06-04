35d7cd0c0f3eb13d4ad0a2e6ef0ec2b5c31fdcf8
Shubham Ugare <shubhamdugare@gmail.com>
2023-06-04 18:28:25 +0000

    Initial commit with llama and human-eval basics

---
+++ README.md
@@ -0,0 +1,10 @@
+Install human-eval: https://github.com/openai/human-eval
+
+Install llama: https://github.com/facebookresearch/llama
+
+Model checkpoint location is hardcoded to: '/share/models/llama_model/llama/' for now 
+
+Run following command 
+```
+python3 -m torch.distributed.launch --nproc_per_node=1 lpw/run_llama.py
+```
---
+++ lpw/test.py
@@ -0,0 +1,11 @@
+from human_eval.data import write_jsonl, read_problems
+
+problems = read_problems()
+
+num_samples_per_task = 200
+samples = [
+    dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
+    for task_id in problems
+    for _ in range(num_samples_per_task)
+]
+write_jsonl("samples.jsonl", samples)
